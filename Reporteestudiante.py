6ac992da86c572ccafe37c597c37bfc20b5df2c1
PepewowPython <jeremyrojas1277@gmail.com>
2026-05-19 07:29:06 -0500

    Correccion de errores

--- Reporteestudiante.py
+++ Reporteestudiante.py
@@ -24,11 +24,11 @@ else:
 	
 	
 print ("------------------------------------------------")
-print (f"| REPORTE DE CALIFICACIONES PERIODO: ", {per}, " 	|")
-print ("+------------------------------------------------+");
-print (f"|Documento; ", {doc}, "						|")
-print (f"|Nombre; ", {nom}, "							|")
-print (f"|Materia; ", {mat}, "							|")
-print (f"|Nota_Definitivo; ", {doc}, "				|")
-print (f"|", {mensaje}, ")									|")
+print (f"| REPORTE DE CALIFICACIONES PERIODO: , {per}, 	|")
+print ("+------------------------------------------------+")
+print (f"|Documento; {doc}       						|")
+print (f"|Nombre; {nom} 		    					|")
+print (f"|Materia; {mat} 			    				|")
+print (f"|Nota_Definitivo; , {doc}		    	    	|")
+print (f"| {mensaje}, ) 					    		|")
 print ("------------------------------------------------")
