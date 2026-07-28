5cf06189badc9f87482f56ed78998008a1ed9172
PepewowPython <jeremyrojas1277@gmail.com>
2026-07-28 06:52:21 -0500

    ajuste en organización

---
+++ segundotrimestre/arreglos.py
@@ -0,0 +1,19 @@
+# @author: Jeremy Chica Tapasco
+# @fecha: 28/07/2026
+# @descripcion: Manejo de arreglos y funciones
+#               Manejo de excepciones.
+#arreglos []: lista, modificarse
+#         (): tupla, no modificable
+
+lista = []
+#agregar un dato
+lista.append(45)
+lista.append("manzana")
+
+#tamaño de la lista
+longitud = len(lista)
+
+print (lista, "numero de elementos: ",longitud)
+
+#eliminar un elemento
+del (lista[1])
